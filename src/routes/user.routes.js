169c1fcbdf0f2c67e83eb75c1e4b205306620d24
Nikhil Kumar <kumarnikhil48578@gmail.com>
2024-02-27 21:01:42 +0530

    add all routes

---
+++ src/routes/user.routes.js
@@ -0,0 +1,33 @@
+import { Router } from "express";
+import {
+    registerUser,
+    loginUser,
+    logoutUser,
+    refreshAccessToken,
+    changeCurrentPassword,
+    getCurrentUser,
+    updateAccountDetails,
+    forgetPassword,
+    passwordReset
+    } from "../controllers/user.controller.js";
+import { verifyJWT } from "../middlewares/auth.middleware.js";
+
+
+const router = Router()
+
+router.route("/register").post(registerUser)
+
+router.route("/login").post(loginUser)
+
+//secured routes
+router.route("/logout").post(verifyJWT,  logoutUser)
+router.route("/refresh-token").post(refreshAccessToken)
+router.route("/change-password").post(verifyJWT, changeCurrentPassword)
+router.route("/current-user").get(verifyJWT, getCurrentUser)
+router.route("/update-account").patch(verifyJWT, updateAccountDetails)
+
+
+router.route("/forgetPassword").post(forgetPassword)
+router.route("/password/reset/:token").post(passwordReset)
+
+export default router
